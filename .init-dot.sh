d5608baa4209a811cce3078826246b70ddcf4f59
John Hui <jzh2106@columbia.edu>
2016-09-25 04:11:03 +0000

    tweaked some safeguards in init

--- .init-dot.sh
+++ .init-dot.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-srcdot='if [ -f ~/.bash_aliases ]; then\n    . ~/.bash_aliases\nfi'
+(
+srcdot='
+if [ -f ~/.bash_aliases ]; then
+        . ~/.bash_aliases
+fi
+'
 
-if [ -w ~/.bashrc ]
+function fresh()
+{
+        [ -w ~/$1 ] || return 0
+        if grep -q '~/.bash_aliases' < ~/.bashrc; then
+                echo "~/.bash_aliases already sourced in $1"
+                exit
+        fi
+        return 1
+}
+
+if fresh .bashrc; then
+        echo -e "${srcdot}" >> ~/.bashrc
+        echo "Wrote to ~/.bashrc"
+elif fresh .bash_profile
 then
-    echo -e $srcdot >> ~/.bashrc
-    echo "Wrote to ~/.bashrc"
-elif [ -w ~/.bash_profile ]
-then
-    echo -e $srcdot >> ~/.bash_profile
-    echo "Wrote to ~/.bash_profile"
+        echo -e "${srcdot}" >> ~/.bash_profile
+        echo "Wrote to ~/.bash_profile"
 else
     echo "No ~/.bashrc or ~/.bash_profile found"
     read -p "Do you want to make one (rc/profile)? " answer
     case ${answer} in
         rc|r|R )
-            echo -e $srcdot >> ~/.bashrc
+            echo -e ${srcdot} >> ~/.bashrc
             echo "Created ~/.bashrc"
         ;;
         profile|p|P )
-            echo -e $srcdot >> ~/.bash_profile
+            echo -e ${srcdot} >> ~/.bash_profile
             echo "Created ~/.bash_profile"
         ;;
         * )
@@ -27,8 +41,14 @@ else
         ;;
     esac
 fi
+)
 
-if [ ! -w ~/.tmux/plugins/tpm ]
+if [ ! -d ~/.tmux/plugins/tpm ]
 then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+if [ ! -d ~/.vim/bundle/nerdcommenter ]
+then
+        git clone https://github.com/scrooloose/nerdcommenter ~/.vim/bundle/nerdcommenter
 fi
