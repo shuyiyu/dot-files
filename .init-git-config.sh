8fb6bb01bb22d4f633c9cd2be2a46def1ad8d5cd
John Hui <jwongsquared@gmail.com>
2016-10-18 02:46:20 -0400

    updated config

--- .init-git-config.sh
+++ .init-git-config.sh
@@ -9,3 +9,9 @@ git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -l HEAD'
 git config --global alias.visual '!gitk'
 
+git config --global alias.am 'commit -am'
+
+git config --global alias.up '!git pull --rebase --prune $@ && git submodule update --init --recursive'
+
+git config --global alias.undo 'reset HEAD~1 --mixed'
+git config --global alias.amend 'commit -a --amend'
